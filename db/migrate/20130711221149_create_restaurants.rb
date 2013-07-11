007e342646dc0fbfe1ed6954c1fe783bde41faf9
timestep <jbosshellokitty@gmail.com>
2013-07-11 18:26:34 -0400

    filled in migration

--- db/migrate/20130711221149_create_restaurants.rb
+++ db/migrate/20130711221149_create_restaurants.rb
@@ -1,6 +1,13 @@
 class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
+    	t.string :name
+    	t.string :address
+    	t.string :neighbourhood
+    	t.integer :price_min
+    	t.integer :price_max
+    	t.text :summary
+    	t.text :menu
 
       t.timestamps
     end
--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 feature "Restaurant Listings" do
 	scenario "Visit Restaurant Listing Page" do
 		visit root_path
+		expect(page.has_selector?('table')).to be_true
 	end
 end
