301a834125ce1491dbd2fcba766405916c4e12b4
Drew Sing <sing.drew@gmail.com>
2013-07-11 19:31:53 -0400

    scenario click on restaurant testing

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "Restaurant Listings" do
+feature "Visitor" do
 	scenario "Visit Restaurant Listing Page" do
 		(1..3).each do |x|
 			FactoryGirl.create(:restaurant)
@@ -10,4 +10,17 @@ feature "Restaurant Listings" do
 		# binding.pry
 		expect(page.all('tr')).to have(3).items
 	end
+
+	scenario "Click on Restaurant" do
+		restaurant = FactoryGirl.create(:restaurant)
+		click_link(restaurant.name)
+
+		expect(page.should render_template restaurant_path)
+		expect(page.has_selector?('table')).to be_true
+
+
+	end 
+
+
+
 end
