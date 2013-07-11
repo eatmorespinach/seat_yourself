79fe1678e23485c15eeacb80ec0d5253db5b2f3b
timestep <jbosshellokitty@gmail.com>
2013-07-11 18:40:05 -0400

    factory girl in test

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 feature "Restaurant Listings" do
 	scenario "Visit Restaurant Listing Page" do
-		Factory
+		(1..3).each do |x|
+			FactoryGirl.create(:restauant)
+		end
 		visit root_path
 		expect(page.has_selector?('table')).to be_true
-
 	end
 end
