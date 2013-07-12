b5a3b591a34c762ae0bdfcf87165b74e86c9280a
Drew Sing <sing.drew@gmail.com>
2013-07-12 19:17:37 -0400

    booking controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 SeatYourself::Application.routes.draw do
+  get "bookings/new"
+  get "bookings/create"
   root 'restaurants#index'
-  resources :restaurants
+
   resources :users
   resources	:sessions
+  resources :restaurants do 
+  	resources :booking
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130712180838) do
+ActiveRecord::Schema.define(version: 20130712230954) do
+
+  create_table "bookings", force: true do |t|
+    t.datetime "date"
+    t.integer  "party"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
