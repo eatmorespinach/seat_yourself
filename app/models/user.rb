a2af39dbaa553665cc63915f49b0d116defb1449
timestep <jbosshellokitty@gmail.com>
2013-07-12 16:35:32 -0400

    passing that testtttttt babbyyyyy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
 	# has_secure_password
   authenticates_with_sorcery!
+  # attr_accessible :password, :password_confirmation
+
+  validates_confirmation_of :password
 end
