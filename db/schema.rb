2a4b3472b5752e9b5cf087626667dc330d33aa4d
timestep <jbosshellokitty@gmail.com>
2013-07-12 14:18:19 -0400

    removed redundant specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130711221149) do
+ActiveRecord::Schema.define(version: 20130712180838) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20130711221149) do
     t.integer  "price_max"
     t.text     "summary"
     t.text     "menu"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
