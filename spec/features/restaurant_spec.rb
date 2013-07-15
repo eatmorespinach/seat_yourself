93e2ad934581f34a0fcb29493c5dd86a8300d2bb
timestep <jbosshellokitty@gmail.com>
2013-07-15 16:15:09 -0400

    testing for restaurant owners

--- spec/features/restaurant_spec.rb
+++ spec/features/restaurant_spec.rb
@@ -1,1 +1,23 @@
-restaurant_spec.rb
+require 'spec_helper.rb'
+
+describe 'Restuarant Owner' do
+	
+	before(:each) do 
+		@restaurant = FactoryGirl.create(:restaurant)
+		@user_attributes = FactoryGirl.attributes_for(:user)
+		@user = User.create(@user_attributes)
+		visit root_path	
+		click_link("Log in")
+		fill_in('Email', :with => @user_attributes[:email])
+		fill_in('Password', :with => @user_attributes[:password])
+		click_button('Log in')
+	end
+
+	context 'click on restaurant profile' do
+		it 'view bookings' do
+			click_link(@restaurant.name)
+			page.should have_text('Bookings')
+			page.should have_text('Party Size')
+		end
+	end
+end
