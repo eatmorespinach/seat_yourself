96dccbafa3b5fd98a419d3f1c854c229033e379b
timestep <jbosshellokitty@gmail.com>
2013-07-15 16:01:13 -0400

    something

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,6 +6,7 @@ FactoryGirl.define do
   	email Faker::Internet.email
   	password 'password'
   	points 10
+    restaurant_id 1
   	# salt{ 'asdasdastr4325234324sdfds' }
   	# crypted_password{
   	#  Sorcery::CryptoProviders::BCrypt.encrypt(
